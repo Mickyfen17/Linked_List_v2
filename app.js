baccac149bcc058306fc756d3e6a790a29421e6f
Michael Fenwick <mickyfen17@aol.com>
2016-12-26 18:09:22 -0700

    Function to send to localStorage and display to DOM

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
+retriveBookmarks();
+
 $("#enter-button").on("click", function() {
-    var titleValue = $("#bookmark-title").val();
-    var contentValue = $("#bookmark-content").val();
-    var uniqueId = Date.now();
-    var newBookmark = new Bookmark(uniqueId, titleValue, contentValue);
-    stringToLocal(uniqueId, newBookmark);
-    // console.log(newBookmark);
-    clearInputs();
+  getInputValues();
+  clearInputs();
 });
 
+function getInputValues() {
+  var titleValue = $("#bookmark-title-input").val();
+  var contentValue = $("#bookmark-content-input").val();
+  var uniqueId = Date.now();
+  var newBookmark = new Bookmark(uniqueId, titleValue, contentValue);
+  stringToLocal(uniqueId, newBookmark);
+}
+
 function clearInputs() {
-  $("#bookmark-title").val("");
-  $("#bookmark-content").val("");
+  $("#bookmark-title-input").val("");
+  $("#bookmark-content-input").val("");
 }
 
 function Bookmark(id, title, content) {
@@ -22,5 +27,24 @@ function Bookmark(id, title, content) {
 function stringToLocal(id, bookmarkObj) {
   var stringBookmark = JSON.stringify(bookmarkObj);
   localStorage.setItem(id, stringBookmark);
-  // console.log(stringBookmark);
+  retriveBookmarks();
+}
+
+function retriveBookmarks() {
+  $(".bookmark-card").remove();
+  for(var key in localStorage) {
+    var parsedBookmark = JSON.parse(localStorage[key]);
+    displayBookmark(parsedBookmark);
+  }
+}
+
+function displayBookmark(bookmark) {
+  $(".bookmark-field").prepend(`
+    <article id="${bookmark.id}" class="bookmark-card">
+      <h3 class="bookmark-title">${bookmark.title}</h3>
+      <h4 class="bookmark-content">${bookmark.content}</h4>
+      <button class="read-button">Read</button>
+      <button class="delete-button">Delete</button>
+    </article>
+  `);
 }
